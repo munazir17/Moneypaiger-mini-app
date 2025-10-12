d2ebffbd1890917f1d307777de569cd8caa341f6
munazir Raza <139262123+munazir17@users.noreply.github.com>
2025-10-12 16:11:03 +0530

    Update and rename vercel.json to vercel.js

---
+++ vercel.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // No configuration needed for static files
+  // .well-known folder will be served automatically
+}
+
+module.exports = nextConfig
