9a1760991b561a5e9ed1e105faa1fa44e90a1bb3
munazir Raza <139262123+munazir17@users.noreply.github.com>
2025-10-12 16:15:23 +0530

    Delete vercel.js

--- vercel.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // No configuration needed for static files
-  // .well-known folder will be served automatically
-}
-
-module.exports = nextConfig
