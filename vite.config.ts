3413b12de413cfc7f672f0bb885ccb66ec9c5e03
munazir Raza <139262123+munazir17@users.noreply.github.com>
2025-10-15 11:11:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
-    host: true
-  }
+    open: true,
+  },
 })
